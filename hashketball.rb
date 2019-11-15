8b89b3f8900e1d06bf666ff3ce1a26553453b373
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 17:43:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,19 +1,18 @@
 def game_hash 
   {
-    :home => {
-      :team_name => "Brooklyn Nets",
-      :colors => ["Black, White"],
-      :players => [
-        {:player_name => "Alan Anderson",
-        :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assists => 12,
-        :steals => 3,
-        :blocks => 1,
-        :slam_dunks => 1 
-      },
+    :home => { :team_name => "Brooklyn Nets",
+                :colors => ["Black, White"],
+                :players => [
+                  {:player_name => "Alan Anderson",
+                    :number => 0,
+                    :shoe => 16,
+                    :points => 22,
+                    :rebounds => 12,
+                    :assists => 12,
+                    :steals => 3,
+                    :blocks => 1,
+                    :slam_dunks => 1 
+                  },
       {:player_name => "Reggie Evans",
         :number => 30,
         :shoe => 14,
