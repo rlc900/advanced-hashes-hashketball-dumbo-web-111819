836eee60799f1857504c1e1ef507a267f69c084a
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 18:12:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,7 +131,17 @@ def num_points_scored(players_name)
 end
 
 def shoe_size(players_name)
-  
+  game_hash.each do |location, team|
+    team.each do |attribute, data|
+     if attribute == :players 
+       data.each do |player|
+         if player[:player_name] == players_name
+           return player[:shoe]
+          end 
+       end 
+      end
+    end 
+  end
 end 
 
 
