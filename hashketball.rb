a54675e6c73a044598d7bc17513f4744cb168d46
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 18:01:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -118,7 +118,9 @@ end
 def num_points_scored(players_name)
   #return number of points of player passed in 
   game_hash.each do |location, team|
-    binding.pry
+    team.each do |attribute, data|
+      binding.pry
+    end 
   end
 end 
 
