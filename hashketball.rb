70de54844b1580c9208908a44afa4e29713bd120
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 18:15:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -146,8 +146,9 @@ end
 
 def team_colors(team_name)
   game_hash.each do |location, team|
-  binding.pry
-      
+  if team[:team_name] == team_name
+     return team[:colors]
+    end 
   end 
 end 
 
