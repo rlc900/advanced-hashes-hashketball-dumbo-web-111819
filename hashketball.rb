56bd5ca7346ee8888084fd873a773fa495293d78
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 17:45:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -55,10 +55,9 @@ def game_hash
                   }
                 ]
               },
-    :away => {
-:team_name => "Charlotte Hornets"
-:colors => ["Turquoise, Purple"]
-:players => [
+    :away => { :team_name => "Charlotte Hornets"
+              :colors => ["Turquoise, Purple"]
+              :players => [
         {:player_name => "Jeff Adrien",
         :number => 4,
         :shoe => 18,
