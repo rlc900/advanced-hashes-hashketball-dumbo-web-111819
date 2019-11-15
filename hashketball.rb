2c03fbf6b8c7cf1c033be1f7e65f9d240e27f789
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 18:09:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -45,7 +45,7 @@ def game_hash
                     :blocks => 8,
                     :slam_dunks => 5 
                   },
-                  {:player_name => "Alan Anderson",
+                  {:player_name => "Jason Terry",
                     :number => 31,
                     :shoe => 15,
                     :points => 19,
