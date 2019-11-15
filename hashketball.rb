64d5316d9e93910f51238342e1bc2638545758a9
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 18:38:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,8 @@ def player_numbers(team_name)
     if team[:team_name] == team_name
      team.each do |attributes, data|
        if attributes == :players
-         data[:number]
+         data.each do |player|
+           player[:number]
        end 
      end 
     end 
