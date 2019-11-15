5b349a81c7c001a45f8df75df887e5ae6edd79c6
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 19:24:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -215,7 +215,7 @@ def most_points_scored
     points = player[:points]
     
     if points > most_points 
-      points = most_points 
+      most_points = points  
     p_with_most_points = player[:player_name]
     binding.pry
     end 
