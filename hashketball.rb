c0f972735aaba89b3704c6084e4c886f7284851c
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-15 18:53:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,12 +181,13 @@ def player_stats(players_name)
       if attributes == :players 
         data.each do |player|
           if player[:player_name] == players_name
-            
+            new_hash = player
           end 
         end 
       end
     end 
   end 
+  new_hash
 end 
 
 
